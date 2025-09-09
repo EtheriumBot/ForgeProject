73275a4eedb931a01138c6c3ec7229a7f855caf2
RSlover521 <wulance58@gmail.com>
2025-09-08 19:20:05 -0500

    halfway finished with worldgen

--- Forge-Project-1.20.X/src/main/java/net/ethan/forgeproject/worldgen/ModBiomeModifiers.java
+++ Forge-Project-1.20.X/src/main/java/net/ethan/forgeproject/worldgen/ModBiomeModifiers.java
@@ -1,18 +1,31 @@
 package net.ethan.forgeproject.worldgen;
 
 import net.ethan.forgeproject.ForgeProject;
+import net.minecraft.core.HolderSet;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BiomeTags;
+import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.common.world.BiomeModifier;
+import net.minecraftforge.common.world.ForgeBiomeModifiers;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class ModBiomeModifiers {
 
+    public static final ResourceKey<BiomeModifier> ADD_SULFUR_ORE = registerKey("add_sulfur_ore");
+
     public static void bootstrap(BootstapContext<BiomeModifier> context) {
         var placedFeature = context.lookup(Registries.PLACED_FEATURE);
         var biomes = context.lookup(Registries.BIOME);
+
+        context.register(ADD_SULFUR_ORE, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
+                biomes.getOrThrow(BiomeTags.IS_OVERWORLD),
+                HolderSet.direct(placedFeature.getOrThrow(ModPlacedFeatures.SULFUR_ORE_PLACED_KEY)),
+                GenerationStep.Decoration.UNDERGROUND_ORES
+        ));
     }
 
     public static ResourceKey<BiomeModifier> registerKey(String name) {
